b34a2886ddc095132bd2384d53beed7af9337203
mehulshetty <54683532+mehulshetty@users.noreply.github.com>
2022-03-05 16:05:26 -0500

    Working Ranked Choice Voting

---
+++ rankedChoice.py
@@ -0,0 +1,67 @@
+import pyrankvote
+import pandas as pd
+import numpy as np
+from pyrankvote import Candidate, Ballot
+
+resultsFile = pd.read_csv("/Users/mehul/Downloads/Marist College School of Liberal Arts Faculty of the Year Voting Form (Responses) - Form Responses 1.csv")
+resultsFile = resultsFile.drop(['Timestamp'], axis=1)
+
+allCandidatesWithDupes = pd.concat([resultsFile['First Choice'], resultsFile['Second Choice'], resultsFile['Third Choice'], resultsFile['Fourth Choice'], resultsFile['Fifth Choice']])
+
+uniqueCandidates = allCandidatesWithDupes.unique()
+
+for i in range(0, uniqueCandidates.__len__()):
+    if(pd.isna(uniqueCandidates[i])):
+        uniqueCandidates = np.delete(uniqueCandidates, i)
+
+# CANDIDATES
+candidates = []
+
+for i in range(0, uniqueCandidates.__len__()):
+    candidates.append(Candidate(uniqueCandidates[i]))
+
+# BALLOTS
+ballots = []
+
+ballotEnd = 0
+
+for i in range(0, resultsFile.size):
+    try:
+        resultsFile.iloc[i]
+        ballotEnd += 1
+    except IndexError as e:
+        break
+
+for i in range(0, ballotEnd):
+    ballotEntry = resultsFile.iloc[i]
+    newBallotEntry = []
+    for x in range(0, 5):
+        if(not pd.isna(ballotEntry.iat[x]) and x == 4):
+            for j in range(0, ballotEntry.size):
+                if(Candidate(ballotEntry.iat[j]) in newBallotEntry):
+                    break
+                else:
+                    newBallotEntry.append(Candidate(ballotEntry.iat[j]))
+            ballots.append(Ballot(ranked_candidates = newBallotEntry))
+            print(newBallotEntry)
+            break
+        if(pd.isna(ballotEntry.iat[x])):
+            ballotEntry = ballotEntry[0:x]
+            for j in range(0, ballotEntry.size):
+                if(Candidate(ballotEntry.iat[j]) in newBallotEntry):
+                    break
+                else:
+                    newBallotEntry.append(Candidate(ballotEntry.iat[j]))
+            ballots.append(Ballot(ranked_candidates = newBallotEntry))
+            print(newBallotEntry)
+            break
+print(resultsFile)
+print(candidates)
+print(ballots)
+# You can use your own Candidate and Ballot objects as long as they implement the same properties and methods
+election_result = pyrankvote.instant_runoff_voting(candidates, ballots)
+
+winners = election_result.get_winners()
+# Returns: [<Candidate('Al Gore (Democratic)')>]
+
+print(election_result)
